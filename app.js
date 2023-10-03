ff6b39c99c7aaabba88df95b47c4d5814b725976
bRad1995 <bradleejwilliams@yahoo.com>
2023-10-03 18:37:20 -0500

    Added  methods to tama class  and additonal images for evolve

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-class Tamagotchi {
+console.log("Tamagotchi")
+
+class Newpet {
     constructor(name, hunger, sleepiness, boredom, age) {
         this.name = name
         this.hunger = hunger
@@ -8,7 +10,7 @@ class Tamagotchi {
     }
 }
 
-class NewPet extends Tamagotchi {
+class Tamagotchi extends Newpet {
     constructor(name, hunger, sleepiness, boredom, age) {
         super(name, hunger, sleepiness, boredom, age)
         this.name = name;
@@ -18,12 +20,95 @@ class NewPet extends Tamagotchi {
         this. age = 0;
     }
     feedTama(){
-
-    }
-    sleepTama(){
-
+        this.hunger -= 1;
+        this.sleepiness += 1;
     }
     boredTama(){
-
+        this.boredom -=2;
+        this.sleepiness +=2;
+        this.hunger++;
     }
+    sleepTama(){
+        this.sleepiness--;
+        this.boredom++;
+    }
+
 }
+
+const tama = new Tamagotchi("", 0, 0, 0, 0)
+
+const hungerLevel = document.querySelector('#hunger')
+const boredomLevel = document.querySelector('#boredom')
+const sleepyLevel = document.querySelector('#sleepiness')
+const startButton = document.querySelector('#Start')
+const feedButton = document.querySelector('#Feed')
+const playButton = document.querySelector('#Play')
+const lightButton = document.querySelector('#Lights')
+
+tamaAge=() => {
+    const intervalID = setInterval(() => {
+    const tamaAge = document.querySelector('#agecounter')
+    tamaAge.innerHTML = `Age: ${tama.age}`
+    tama.age++
+
+    if(tama.age == 1){
+        alert(tama.name + " is fixing to hatch!")
+        document.querySelector('#tamaImg').src="https://img.itch.zone/aW1hZ2UvODE2MTk0LzQ1OTgzNDMuZ2lm/original/KV3fcg.gif"
+        alert(tama.name + " is now a child!")
+    }
+    if(tama.age == 3){
+        alert(tama.name + " is fixing to evolve!")
+        document.querySelector('#tamaImg').src="https://www.icegif.com/wp-content/uploads/2022/02/icegif-942.gif"
+    }
+
+}, 10000)
+    }
+tamaAge()    
+
+tamaHunger=() => {
+const intervalID = setInterval(() => {
+    const hungryTamaID = document.querySelector('#hunger')
+    // console.log(hungryTamaID)
+    tama.hunger= parseInt(tama.hunger) + 1
+    hungryTamaID.value = tama.hunger
+    // console.log(tama.hunger)
+
+    if(tama.hunger > 10){
+        alert("Oh no! " + tama.name + " died from starvation :( !")
+        clearInterval(intervalID)
+        return
+    }
+}, 3000)
+}
+tamaHunger()
+
+tamaBoredom=() => {
+    const intervalID = setInterval(() => {
+        const boredTamaID = document.querySelector('#boredom')
+        tama.boredom= parseInt(tama.boredom) + 1
+        boredTamaID.value = tama.boredom
+    
+        if(tama.boredom > 10){
+            alert("Oh no! " + tama.name + " died from lack of attention :( !")
+            clearInterval(intervalID)
+            return
+        }
+    }, 8000)
+    }
+    tamaBoredom()
+
+    tamaSleepiness=() => {
+        const intervalID = setInterval(() => {
+            const sleepyTamaID = document.querySelector('#sleepiness')
+            tama.sleepiness= parseInt(tama.sleepiness) + 2
+            sleepyTamaID.value = tama.sleepiness
+        
+            if(tama.sleepiness > 10){
+                alert("Oh no! " + tama.name + " died from lack of sleep :( !")
+                clearInterval(intervalID)
+                return
+            }
+        }, 6000)
+        }
+        tamaSleepiness()
+    
